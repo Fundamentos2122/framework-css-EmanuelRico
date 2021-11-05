3c7f435f8d8526d67c5b20a6517c01209a46a31d
Emanuel Rico <emanuelrico8@gmail.com>
2021-11-04 18:59:01 -0600

    Se modifica el menu del framework

--- js/framework.js
+++ js/framework.js
@@ -2,21 +2,41 @@ const attr_toggle = 'data-toggle';
 const attr_target = 'data-target';
 const attr_dismiss = 'data-dismiss';
 const class_modal = 'modal';
+const dropdown_class = 'dropdown';
+const dropdown_toggle = 'dropdown-toggle';
+const dropdown_menu_class = 'dropdown-menu';
+const navbar_toggle = 'navbar-toggle';
+const menu_collapse_class = 'navbar-collapse';
 const class_show = 'show';
+
 
 document.addEventListener('DOMContentLoaded', function(){
     //Botones que abren un modal
-    let modal_open_buttons = document.querySelectorAll(`[${attr_toggle}='${class_modal}']`)
+    let modal_open_buttons = document.querySelectorAll(`[${attr_toggle}='${class_modal}']`);
 
     modal_open_buttons.forEach(element => {
         element.addEventListener('click', OpenModal);
     });
 
     //Botones que cierran un modal
-    let modal_close_buttons = document.querySelectorAll(`[${attr_dismiss}]`)
+    let modal_close_buttons = document.querySelectorAll(`[${attr_dismiss}]`);
 
     modal_close_buttons.forEach(element => {
         element.addEventListener('click', CloseModal);
+    });
+
+    //Dropdown del submenú
+    let dropdown_buttons = document.querySelectorAll(`.${dropdown_class} > .${dropdown_toggle}`);
+
+    dropdown_buttons.forEach(element => {
+        element.addEventListener('click', ToggleDropdown);
+    });
+
+    //Collapse del menú
+    let collapse_menu_buttons = document.querySelectorAll(`.${navbar_toggle}`);
+
+    collapse_menu_buttons.forEach(element => {
+        element.addEventListener('click', ToggleMenu);
     });
 });
 
@@ -48,4 +68,28 @@ function OpenModal(e){
     
     //Agregar la clase para mostrar el modal
     modal.classList.remove(class_show);
+}
+
+/**
+ * Abrir y cerrar un submenú
+ * @param {PointerEvent} e 
+ */
+ function ToggleDropdown(e){
+    e.preventDefault();
+
+    let submenu = e.target.parentNode.querySelector(`.${dropdown_menu_class}`);
+
+    submenu.classList.toggle(class_show);
+}
+
+/**
+ * Abrir y cerrar un submenú
+ * @param {PointerEvent} e 
+ */
+ function ToggleMenu(e){
+    e.preventDefault();
+
+    let menu = e.target.parentNode.querySelector(`.${menu_collapse_class}`);
+
+    menu.classList.toggle(class_show);
 }
